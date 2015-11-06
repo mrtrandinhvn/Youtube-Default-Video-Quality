4e7664f65f22826b1dd3e5b21f08f0b8b57f5c5f
dinhtran <mrtrandinhvn@gmail.com>
2015-11-07 01:59:10 +0700

    Version 1.0.0.0.0.0.0.0.0.0: the first stable version

--- js/contentscriptjs/commonfns.js
+++ js/contentscriptjs/commonfns.js
@@ -2,26 +2,20 @@
 function isArray(object) {
     return Object.prototype.toString.call(object) === "[object Array]";
 }
+//#region ============ Utillities ===========
 
+// load youtube settings
 function loadSettings(chromeStorage, callback) {
-    chromeStorage.get(null, function (data) {
+    chromeStorage.get(null , function(data) {
         if (data.youtubeSettingsObj) {
             youtubeSettingsObj = data.youtubeSettingsObj;
         }
-		// execute callback function after finish loading settings
-		if(typeof(callback) == "function"){
-			callback();
-		}
-    });
-}
-function injectCodeFromSourceFileToBackgroundPage(filePaths) {
-    if (isArray(filePaths)) {
-        for (var i = 0; i < filePaths.length; i++) {
-            var script = document.createElement('script');
-            script.src = chrome.extension.getURL(filePaths[i]);
-            (document.head || document.documentElement).appendChild(script);
+        // execute callback function after finish loading settings
+        if (typeof (callback) == "function") {
+            callback();
         }
     }
+    );
 }
 
 function injectScriptToBackgroundPage(scripts) {
@@ -29,9 +23,75 @@ function injectScriptToBackgroundPage(scripts) {
         for (var i = 0; i < scripts.length; i++) {
             var script = document.createElement('script');
             script.textContent = scripts[i];
-			
-            (document.head || document.documentElement).appendChild(script); // inject scripts to <head>
-			script.parentNode.removeChild(script); // remove injected scripts
+            
+            (document.head || document.documentElement).appendChild(script);
+            // inject scripts to <head>
+            script.parentNode.removeChild(script);
+            // remove injected scripts
         }
     }
 }
+//#endregion ========= Utillities ===========
+
+
+//#region ============= Player Controls =============
+// order of those 2 array must be identical
+var qualityValues = [
+"default", 
+"tiny", 
+"small", 
+"medium", 
+"large", 
+"hd720", 
+"hd1080", 
+"highres"
+];
+var qualityNumberText = [
+"Auto", "144p", "240p", "360p", "480p", "720p", "1080p", "2160p"
+];
+
+
+function showAnnotation(show) {
+    if (show === false) {
+        document.querySelector("div.video-annotations").style.display = "none";
+    }
+}
+function setVideoQuality(qualityValue) {
+    var qualityArrayIndex = getQualityIndex(qualityValue);
+    document.querySelectorAll("button.ytp-button.ytp-settings-button")[0].click();
+    document.querySelectorAll(".ytp-menuitem[aria-haspopup=true] .ytp-menuitem-content")[1].click();
+    document.querySelectorAll(".ytp-popup.ytp-settings-menu")[0].style.display = "none";
+    // hide quality selecting menu
+    setTimeout(function() {
+        var qualityOptions = document.querySelectorAll(".ytp-menu.ytp-quality-menu")[0].querySelectorAll(".ytp-menuitem");
+        var isDone = false;
+        // video might not support the selected value
+        while (!isDone) {
+            for (var i = 0; i < qualityOptions.length; i++) {
+                var qualityNumberSpan = qualityOptions[i].querySelectorAll("span")[0];
+                if (qualityNumberSpan.textContent.includes(qualityNumberText[qualityArrayIndex])) {
+                    qualityNumberSpan.click();
+                    isDone = true;
+                    break;
+                }
+            }
+            qualityArrayIndex--;
+        }
+    }
+    , 300);
+}
+function getQualityIndex(qualityValue) {
+    for (var i = 0; i < qualityValues.length; i++) {
+        if (qualityValues[i] === qualityValue) {
+            return i;
+        }
+    }
+    return 0;
+}
+function isPlayerReady() {
+    if (movie_player.getElementsByTagName("video")[0].src) {
+        return true;
+    }
+    return false;
+}
+//#endregion ========== Player Controls =============
--- js/contentscriptjs/main.js
+++ js/contentscriptjs/main.js
@@ -2,69 +2,52 @@
 // quality option elements
 var qualityValueElements;
 // local setting object
-var youtubeSettingsObj = { defaultQualty: "default" };
+var youtubeSettingsObj = {
+    defaultQualty: "default"
+};
 // chrome storage object
 var chromeStorage = chrome.storage.sync;
 // #endregion ======= END Key Objects =========
 
 // #region ====== Executing =========
-(function () {
+(function() {
+    
     // execute after settings are fully loaded
     function doMain() {
-        // inject code to background page.
-        //var scriptList = ["js/contentscriptjs/execute.js"];
-        var injectScriptList = [];
-        var executeScript = "(" + // stringify script
-            function doMain(youtubeSettingsObj) {
-                var playerList = document.getElementsByClassName('html5-video-player');
-                if (playerList.length == 0) {
-                    var findPlayerListInterval = setInterval(function () {
-                        debugger;
-                        playerList = document.getElementsByClassName('html5-video-player');
-                        if (playerList.length > 0) {
-                            doMain(youtubeSettingsObj);
-                            clearInterval(findPlayerListInterval);
-                        }
-                    }, 1000);
-                }
-                var playerState = {
-                    // youtube player's states
-                    UNSTARTED: -1,
-                    END: 0,
-                    PLAYING: 1,
-                    PAUSED: 2,
-                    BUFFERING: 3,
-                    CUED: 5
-                }
-                var mainPlayer; // running player
-                var onPlayerStateChange = function (state) {
-                    console.log(state);
-                    if ((state == playerState.PLAYING)) {
-                        mainPlayer.setPlaybackQuality(youtubeSettingsObj.defaultQualty);
-                    }
-                };
-                for (var i = 0; i < playerList.length; i++) {
-                    playerList[i].addEventListener("onStateChange", onPlayerStateChange);
-                    // set replay
-                    playerList[i].getElementsByTagName("video").loop = youtubeSettingsObj.replay;
-                    // set video quality
-                    playerList[i].setPlaybackQuality(youtubeSettingsObj.defaultQualty);
-                    if (playerList[i].getPlayerState() != -1) {
-                        mainPlayer = playerList[i];
-                        mainPlayer.pauseVideo();
-                        mainPlayer.setPlaybackQuality(youtubeSettingsObj.defaultQualty);
-                        mainPlayer.seekTo(mainPlayer.getCurrentTime(), true);
-                        mainPlayer.playVideo();
+        var firstRun = setInterval(function() {
+            // check if player is ready every 500ms
+            if (isPlayerReady()) {
+                setVideoQuality(youtubeSettingsObj.defaultQualty);
+                showAnnotation(youtubeSettingsObj.showAnnotation);
+                clearInterval(firstRun);
+            }
+        }
+        , 500);
+        // create an observer instance for video player to check if video.src has changed
+        var target = movie_player.getElementsByTagName("video")[0];
+        var observer = new MutationObserver(function(mutations) {
+            mutations.forEach(function(mutation) {
+                if (mutation.attributeName === "src") {
+                    if (mutation.target.src) {
+                        // Apply Settings when player src is changed
+                        setVideoQuality(youtubeSettingsObj.defaultQualty);
                     }
                 }
             }
-            + ")(" + JSON.stringify(youtubeSettingsObj) + ");";
-        injectScriptList.push(executeScript); // add to inject code list
-        // inject code to background page
-        injectScriptToBackgroundPage(injectScriptList);
+            );
+        }
+        );
+        
+        // configuration of the observer:
+        var config = {
+            attributes: true
+        };
+        
+        // pass in the target node, as well as the observer options
+        observer.observe(target, config);
     }
-
     // load settings then execute doMain callback function
     loadSettings(chromeStorage, doMain);
-})();
+}
+)();
 // #endregion ====== END Executing =========
